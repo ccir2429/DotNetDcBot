8c56386bded19d4bf5efa21903499f24bee341dc
cosma.cosmin <cosma.cosmin@PC310767>
2020-01-06 15:44:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDcBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+        }
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+            string botPrefix = "";
+        }
+    }
+}
